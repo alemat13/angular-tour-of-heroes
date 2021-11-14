a5287d810537cfed5ae1e6f996884da5141913a9
alemat13 <alemat13@gmail.com>
2021-11-14 22:41:11 +0100

    Add a new hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,7 +12,13 @@ import { MessageService } from './message.service';
   providedIn: 'root'
 })
 export class HeroService {
+
   private heroesUrl = 'api/heroes';
+
+  httpOptions = {
+    headers: new HttpHeaders({'Content-type': 'application/json'})
+  }
+
   constructor(
     private http: HttpClient,
     private messageService: MessageService
@@ -25,6 +31,7 @@ export class HeroService {
         catchError(this.handleError<Hero[]>('getHeroes', []))
       );
   }
+
   getHero(id: number): Observable<Hero> {
     const url=`${this.heroesUrl}/${id}`;
     return this.http.get<Hero>(url).pipe(
@@ -32,9 +39,25 @@ export class HeroService {
       catchError(this.handleError<Hero>(`getHero id=${id}`))
     );
   }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(newHero => this.log(`added hero id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
+
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       console.log(error);
